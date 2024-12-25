d95c47c51c84652db1c23c0058fccb6762fce179
M397 <m397.dev@gmail.com>
2024-12-26 03:33:17 +0700

    feat: Add html-template

---
+++ src/Controller/Home.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+/**
+ * Class Home.
+ *
+ * This is the default controller for this application.
+ */
+#[Route('/', name: 'app_home_')]
+class Home extends BaseController
+{
+
+    /**
+     * Action index.
+     *
+     * @return Response
+     */
+    #[Route(name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+
+}
