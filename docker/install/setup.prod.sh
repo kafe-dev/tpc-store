9c4bb0b006db1d1169d55ef68d711ff5e40a133f
M397 <m397.dev@gmail.com>
2024-12-28 00:53:21 +0700

    fix: Fix docker install

--- docker/install/setup.prod.sh
+++ docker/install/setup.prod.sh
@@ -5,11 +5,9 @@ echo "* * * * * cd /var/www/tpc-store && git pull" | tee -a /var/spool/cron/cron
 echo "0 3 * * * cd /var/www/tpc-store && docker-compose up -d" | tee -a /var/spool/cron/crontabs/root
 
 # Setup project
+cd /var/www/tpc-store || exit
 chmod -R 777 var
 composer install
-
-# Run docker
-cd /var/www/tpc-store || exit
 cp -r docker/docker-compose.prod.yml ./docker-compose.yml
 cp -r docker/Dockerfile ./Dockerfile
 docker-compose up -d
