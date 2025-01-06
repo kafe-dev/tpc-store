322458f0887e306d95c16fdbbe33fc7675c649c5
hoep <hiepnguyen3624@gmail.com>
2025-01-06 15:18:20 +0700

    update:add a controller for setting crud

---
+++ src/Controller/Admin/Setting.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @project tpc-store
+ * @author hoepjhsha
+ * @email hiepnguyen3624@gmail.com
+ * @date 06/01/2025
+ * @time 15:05
+ */
+
+namespace App\Controller\Admin;
+
+class Setting
+{
+
+}
