f0f9b4263ae76f239f8b7fc3c0e71c85efb51f75
Tuan Anh <tuananh182004@gmail.com>
2025-01-08 19:07:11 +0700

    update full
    update full

--- src/Controller/Admin/SaleEventController.php
+++ src/Controller/Admin/SaleEventController.php
@@ -14,14 +14,23 @@ namespace App\Controller\Admin;
 
 use App\Contract\Crud\CrudInterface;
 use App\Controller\BaseController;
+use App\Entity\ProductVariant;
+use App\Entity\ProductVariantSaleEvent;
 use App\Entity\SaleEvent;
+use App\Entity\SaleEventMeta;
 use App\Form\Type\SaleEventType;
+use App\Repository\ProductVariantRepository;
+use App\Repository\ProductVariantSaleEventRepository;
+use App\Repository\SaleEventMetaRepository;
+use App\Repository\SaleEventRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Flasher\Prime\FlasherInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 /**
  * Class SaleEventController.
@@ -32,17 +41,68 @@ use Flasher\Prime\FlasherInterface;
 #[Route(path: '/admin/sale_event', name: 'admin_sale_event_')]
 class SaleEventController extends BaseController implements CrudInterface
 {
+
+    /**
+     * Sale Event Repository
+     *
+     * @var SaleEventRepository $saleEventRepository
+     */
+    private SaleEventRepository $saleEventRepository;
+    /**
+     * Product Variant Sale Event Repository
+     *
+     * @var ProductVariantSaleEventRepository $productVariantSaleEventRepository
+     */
+    private ProductVariantSaleEventRepository $productVariantSaleEventRepository;
+    /**
+     * Sale Event Meta Repository
+     *
+     * @var SaleEventRepository $saleEventMetaRepository
+     */
+    private SaleEventMetaRepository $saleEventMetaRepository;
+    /**
+     * Product Variant Repository
+     *
+     * @var ProductVariantRepository $productVariantRepository
+     */
+    private ProductVariantRepository $productVariantRepository;
+    private SerializerInterface $serializer;
+    public function __construct(EntityManagerInterface $em, SerializerInterface $serializer)
+    {
+        parent::__construct($em, $serializer);
+
+        $this->saleEventRepository = $em->getRepository(SaleEvent::class);
+        $this->productVariantSaleEventRepository = $em->getRepository(ProductVariantSaleEvent::class);
+        $this->saleEventMetaRepository = $em->getRepository(SaleEventMeta::class);
+        $this->productVariantRepository = $em->getRepository(ProductVariant::class);
+        $this->serializer = $serializer;
+    }
+
+
     /**
      * Return list of settings in json format.
      *
      * @return Response
      */
-    #[Route('/getList', name: 'get_list', methods: ['GET'])]
+    #[Route('/list', name: 'list', methods: ['GET'])]
     public function list(): JsonResponse
     {
         $sale_event = $this->em->getRepository(SaleEvent::class)->findAll();
 
-        return $this->json($sale_event);
+        $data = [];
+        foreach ($sale_event as $event) {
+            $data[] = [ 'id' => $event->getId(),
+                        'name' => $event->getName(),
+                        'discountAmount' => $event->getDiscountAmount(),
+                        'discountType' => $event->getDiscountType(),
+                        'startAt' => $event->getStartAt(),
+                        'endAt' => $event->getEndAt(),
+                        'createdAt' => $event->getCreatedAt(),
+                        'updatedAt' => $event->getUpdatedAt(),
+                     ];
+        }
+
+        return $this->json($data);
     }
 
 
@@ -73,8 +133,18 @@ class SaleEventController extends BaseController implements CrudInterface
     {
         $task = $this->em->getRepository(SaleEvent::class)->find($id);
 
+        $metaData = $this->saleEventMetaRepository->findMetaBySaleEventId($id);
+
+        $all = $this->productVariantSaleEventRepository->findProductVariantsBySaleEventId($id);
+        $productVariants = [];
+        foreach ($all as $productVariantSaleEvent) {
+            $productVariants[] = $productVariantSaleEvent->getProductVariant();
+        }
+
         return $this->render('admin/sale_event/detail.html.twig', [
             'page_title' => 'Chi tiết sự kiện sale',
+            'meta_data' => $metaData,
+            'productVariants' => $productVariants,
             'sale_event' => $task,
         ]);
     }
@@ -150,17 +220,12 @@ class SaleEventController extends BaseController implements CrudInterface
 
                     flash()->success('Cập nhật sự kiện Sale thành công!');
                     return $this->redirectToRoute('admin_sale_event_index');
-                } catch (Exception $e) {
-                    flash()->error('Cập nhật sự kiện Sale thất bại!');
-
-                    flash()->error($e->getMessage());
+                } catch (\Exception $e) {
+                    flash()->error('Cập nhật sự kiện Sale thất bại! Error: ' . $e->getMessage());
                 }
+                return $this->redirectToRoute('admin_sale_event_update', ['id' => $id]);
             } else {
                 flash()->error('Cập nhật sự kiện Sale thất bại!');
-
-                foreach ($form->getErrors(true) as $error) {
-                    $this->addFlash('error', $error->getMessage());
-                }
             }
         }
 
@@ -191,6 +256,58 @@ class SaleEventController extends BaseController implements CrudInterface
         }
 
         return $this->redirectToRoute('admin_sale_event_index');
+    }
+
+    /**
+     * Persist Data before flushing.
+     *
+     * @param SaleEvent $saleEvent
+     * @param $form
+     * @param bool $needPersist
+     * @return void
+     */
+    private function persistData(SaleEvent $supplier, $form, bool $needPersist = false): void
+    {
+        $supplier->setName($form->get('name')->getData());
+
+        if ($needPersist === true) {
+            $this->em->persist($supplier);
+        }
+    }
+
+    /**
+     * Persist MetaData before flushing.
+     *
+     * @param SupplierEntity $supplier
+     * @param array $metaData
+     * @param bool $needPersist
+     * @return void
+     */
+    private function persistMetaData(SupplierEntity $supplier, array $metaData, bool $needPersist = false): void
+    {
+        $existingSupplierMeta = $this->supplierMetaRepository->findBy(['supplier' => $supplier->getId()]);
+        $metaMap = [];
+
+        foreach ($existingSupplierMeta as $meta) {
+            $metaMap[$meta->getMetaKey()] = $meta;
+        }
+
+        foreach ($metaData as $key => $value) {
+            if (isset($metaMap[$key])) {
+                // Update existing meta
+                $metaMap[$key]->setMetaValue([$value]);
+            } else {
+                // Create new meta if it doesn't exist
+                $newMeta = new SupplierMetaEntity();
+                $newMeta->setSupplier($supplier);
+                $newMeta->setMetaKey($key);
+                $newMeta->setMetaValue([$value]);
+
+                if ($needPersist === true) {
+                    $this->em->persist($newMeta);
+                }
+            }
+        }
     }
 
 }
